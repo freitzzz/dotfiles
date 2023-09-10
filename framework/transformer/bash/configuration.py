d8b6ce0cfe806018139e936d97a5daaed1f4513a
freitzzz <1160907@isep.ipp.pt>
2023-09-10 14:08:18 +0100

    test

--- framework/transformer/bash/configuration.py
+++ framework/transformer/bash/configuration.py
@@ -16,6 +16,8 @@ class AliasModuleConverter(ModuleConverter[AliasModule]):
                 "alias_source=~/.bash_aliases",
                 "source $alias_source",
                 *map(lambda e: f"export {e[0]}='{e[1]}'", _input.entries.value.items()),
+                "alias",
+                "echo '----------------'"
                 "alias > $alias_source"
             ]
         )
