03ab522d52f50c60d678d7f80670f1c7ca05fa6e
freitzzz <1160907@isep.ipp.pt>
2023-09-10 10:45:13 +0100

    fix: missing literal ' in alias definition

--- framework/transformer/bash/configuration.py
+++ framework/transformer/bash/configuration.py
@@ -11,7 +11,7 @@ class AliasModuleConverter(ModuleConverter[AliasModule]):
     """
 
     def convert(self, _input: AliasModule) -> Bash:
-        return join_lines(map(lambda e: f"export {e[0]}={e[1]}", _input.entries.value.items()))
+        return join_lines(map(lambda e: f"export {e[0]}='{e[1]}'", _input.entries.value.items()))
 
     def module_type(self) -> ModuleType:
         return ModuleType.alias
