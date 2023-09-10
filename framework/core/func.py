96f95cfa0e2e26d12daa91f8b20d6975693da175
freitzzz <1160907@isep.ipp.pt>
2023-09-10 10:07:45 +0100

    refactor: remove unused function

--- framework/core/func.py
+++ framework/core/func.py
@@ -15,21 +15,6 @@ def first(iterable: set[X] | list[X], where: Callable[[X], bool]) -> X:
     return next(filter(where, iterable))
 
 
-def fold(iterable: set[X] | list[X], combine: Callable[[Y, X], Y], initial: Y):
-    """
-    Folds all elements of a set/list in a single value.
-
-    :param iterable: the set/list to fold.
-    :param combine: the folding function, combining the previous value with the next item in the set/list.
-    :param initial: the initial value to be combined with the first item.
-    :return: the folded value.
-    """
-    value = initial
-    for item in iterable:
-        value = combine(value, item)
-    return value
-
-
 def join_lines(iterable: Iterable[str]):
     """
     Combines a group of lines in a single string.
