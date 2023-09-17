f22779f7ba15738792b763d87cb4ee0b558c479e
freitzzz <1160907@isep.ipp.pt>
2023-09-17 13:36:35 +0100

    test

--- install.py
+++ install.py
@@ -56,7 +56,7 @@ class Installer:
         bash_script = self.bash_module_factory.create(module)
         bash_script_b64 = base64.b64encode(bytes(bash_script, "utf-8")).decode("utf-8")
         _exit_code = os.system(
-            f'echo "{bash_script_b64}" | base64 -d -'
+            f'echo "{bash_script_b64}" | base64 -d - | echo "yay"'
         )
 
         if _exit_code == 0:
