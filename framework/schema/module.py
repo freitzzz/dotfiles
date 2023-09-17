92563e7efa89f6e0d009d0efe7d774cd4d0172cd
freitzzz <1160907@isep.ipp.pt>
2023-09-17 19:10:20 +0100

    test

--- framework/schema/module.py
+++ framework/schema/module.py
@@ -72,7 +72,7 @@ class Module(ObjectElement):
         self.dependencies = safe_set(dependencies)
 
     def __hash__(self):
-        return hash(self.name) + hash(self.type)
+        return hash(self.name) + hash(self.type.value)
 
     def __eq__(self, other):
         return self.__hash__() == hash(other)
