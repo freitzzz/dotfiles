6cd067e8b92dae636002d1ae502f86f67c710aa9
freitzzz <1160907@isep.ipp.pt>
2023-09-20 19:32:07 +0100

    test

--- install.py
+++ install.py
@@ -50,7 +50,7 @@ class Installer:
         self.modules_to_install = self._load_modules_to_install()
 
     def run(self):
-        abc = list(self.modules_to_install.intersection(self.installed_modules))
+        abc = list(self.modules_to_install.difference(self.installed_modules))
 
         for module in self.modules_to_install:
             print(f"MODULE TO INSTALL: {module}")
