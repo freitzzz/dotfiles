9d08b4ac08859017c5dc9d4e0df1551b1a111b71
freitzzz <1160907@isep.ipp.pt>
2023-09-22 20:05:14 +0100

    test

--- framework/core/const.py
+++ framework/core/const.py
@@ -1,5 +1,5 @@
 # Default directory where dotfiles configuration files live
-configuration_directory_path = '~/.dotfiles'
+configuration_directory_path = '~/home/freitas/.dotfiles'
 
 # Location of barrel file that sources all dotfiles configuration files
 dotfiles_init_path = f'{configuration_directory_path}/init'
