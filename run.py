fd9ec053c788c314e2823039d16c28932ecf8233
freitzzz <1160907@isep.ipp.pt>
2023-10-15 15:58:56 +0100

    test

--- run.py
+++ run.py
@@ -1,9 +1,34 @@
 #!/usr/bin/env python3
+import argparse
+
 from framework.core.const import installed_modules_path
 from framework.core.log import put_snakes_to_work, ConsoleLumberSnakeClient
 from framework.installer.installer import Installer
 from framework.transformer.bash import bash_module_factory
 from framework.transformer.json import json_module_factory, json_profile_factory
+
+argument_parser = argparse.ArgumentParser(
+    description="Configures modules using dotfiles framework",
+)
+
+argument_parser.add_argument(
+    "-d",
+    "--directory",
+    help="path to a directory that contains module files to load by the framework",
+    default=".",
+    required=False
+)
+
+argument_parser.add_argument(
+    "-p",
+    "--profile",
+    help="path to a profile file that specifies a set of modules to configure",
+    required=False
+)
+
+args = argument_parser.parse_args()
+
+print(args)
 
 put_snakes_to_work(
     [
@@ -13,7 +38,7 @@ put_snakes_to_work(
 
 installer = Installer(
     installed_modules_directory=installed_modules_path,
-    modules_directory=".",
+    modules_directory=args,
     profile_path="profiles/personal.json",
     _json_module_factory=json_module_factory,
     _bash_module_factory=bash_module_factory,
