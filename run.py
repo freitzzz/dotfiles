f764e3b413a398be2489e80a1c05490a415633fb
freitzzz <1160907@isep.ipp.pt>
2023-10-15 12:55:40 +0100

    test

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from framework.core.const import installed_modules_path
 from framework.core.log import put_snakes_to_work, ConsoleLumberSnakeClient
 from framework.installer.installer import Installer
 from framework.transformer.bash import bash_module_factory
-from framework.transformer.json import json_module_factory
+from framework.transformer.json import json_module_factory, json_profile_factory
 
 put_snakes_to_work(
     [
@@ -12,10 +12,13 @@ put_snakes_to_work(
 )
 
 installer = Installer(
-    installed_modules_path,
-    ".",
-    json_module_factory,
-    bash_module_factory,
+    installed_modules_directory=installed_modules_path,
+    modules_directory=".",
+    profile_path="profiles/personal.json",
+    _json_module_factory=json_module_factory,
+    _bash_module_factory=bash_module_factory,
+    _json_profile_factory=json_profile_factory,
+
 )
 
 installer.run()
