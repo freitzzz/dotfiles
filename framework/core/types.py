a736e36122dd6188fe8f02a59d9f72d138842164
freitzzz <1160907@isep.ipp.pt>
2023-12-19 15:08:33 +0000

    test

--- framework/core/types.py
+++ framework/core/types.py
@@ -149,7 +149,7 @@ class Factory(Generic[TI, TO]):
         print(type(_input))
         a = first(self.converters, lambda c: c.accepts(_input), or_else=None)
         print(a)
-        if dict(_input).get("name") == "personal":
+        if dict(_input).get("name") == "personal" and dict(_input).get("type") == "alias":
             raise Exception("!")
         return a.convert(_input)
 
