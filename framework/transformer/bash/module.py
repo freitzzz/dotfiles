284551baec46d22901d3be77f1bdf105a7bde804
freitzzz <1160907@isep.ipp.pt>
2023-12-20 14:20:19 +0000

    test

--- framework/transformer/bash/module.py
+++ framework/transformer/bash/module.py
@@ -14,6 +14,7 @@ class ModuleConverter(BashConverter[M], Generic[M], ABC):
 
     def accepts(self, _input: M) -> bool:
         print(f"??? ({_input.name}) -> {_input.type}")
+        print(dict(_input))
         return _input.type == self.module_type()
 
     @abstractmethod
