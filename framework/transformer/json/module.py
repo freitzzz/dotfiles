dcbc8131be2ceafb7191fdeca9387036a48f53c2
freitzzz <1160907@isep.ipp.pt>
2023-12-20 14:27:33 +0000

    test

--- framework/transformer/json/module.py
+++ framework/transformer/json/module.py
@@ -1,4 +1,3 @@
-import traceback
 from abc import abstractmethod
 from typing import TypeVar, Generic
 
@@ -40,7 +39,7 @@ class ModuleConverter(JsonConverter[M], Generic[M]):
             print(_input.get('type'))
             print(_input.get('name'))
             print(_input.keys())
-            print(traceback.format_exc())
+            raise Exception("")
 
         return _input.get('type') == self.module_type().value and _input.get('name') is not None
 
