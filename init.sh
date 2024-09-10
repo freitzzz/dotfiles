911032e9e04a1e6aa4e130e02fbe5bf1b9d1bf37
João Freitas <1160907@isep.ipp.pt>
2024-09-10 11:27:15 +0100

    feat: add fonts, alacritty, picom

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-dirs=(".config/awesome" ".config/rofi" ".local/bin")
+dirs=(".config/awesome" ".config/rofi" ".config/picom" ".config/alacritty" ".local/bin" ".fonts")
 
 for dir in ${dirs[@]}; do
     sudo mount --bind ~/$dir/ $dir
