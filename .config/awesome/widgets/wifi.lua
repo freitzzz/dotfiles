70aea19048005ba9f908e98664310101119cb76c
João Freitas <1160907@isep.ipp.pt>
2024-09-11 13:52:33 +0100

    refactor(ui): use schedule function

--- .config/awesome/widgets/wifi.lua
+++ .config/awesome/widgets/wifi.lua
@@ -3,36 +3,31 @@
 local assets = require 'assets'
 local button = require 'widgets.button'
 local commands = require 'commands'
-local gears = require 'gears'
+local foundation = require 'foundation'
 
 local self = {}
-local ssid_query_timer
 
 local function updateButton(btn)
     commands.wifi_enabled(function(out)
         if out == 'enabled' then
-            commands.wifi_ssid(function(ssid)
-                if ssid == '' then
-                    ssid_query_timer = gears.timer {
-                        timeout   = 3,
-                        call_now  = false,
-                        autostart = true,
-                        callback  = function()
-                            commands.wifi_ssid(function(ssid)
-                                if not (ssid == '')
-                                then
-                                    button.update(btn, { text = ssid, icon = assets.icons.wifi })
-                                    -- cleanup (use watch)
-                                    ssid_query_timer:stop()
-                                    ssid_query_timer = nil
-                                end
-                            end)
+            foundation.schedule {
+                timeout = 3,
+                autostart = true,
+                continuous = true,
+                instant = true,
+                callback = function(timer)
+                    commands.wifi_ssid(function(ssid)
+                        if not (ssid == '')
+                        then
+                            button.update(btn, { text = ssid, icon = assets.icons.wifi })
+
+                            if timer then
+                                timer:stop()
+                            end
                         end
-                    }
-                else
-                    button.update(btn, { text = ssid, icon = assets.icons.wifi })
+                    end)
                 end
-            end)
+            }
         else
             button.update(btn, { icon = assets.icons.wifi_off, text = '' })
         end
