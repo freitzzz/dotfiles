6313b3acfc9d86f95db481c2e2691f1075c6a8d3
João Freitas <1160907@isep.ipp.pt>
2024-09-11 11:46:20 +0100

    refactor(ui): use schedule function on calendar

--- .config/awesome/widgets/calendar.lua
+++ .config/awesome/widgets/calendar.lua
@@ -3,6 +3,7 @@
 local assets = require 'assets'
 local button = require 'widgets.button'
 local gears = require 'gears'
+local foundation = require 'foundation'
 
 local self = {}
 
@@ -10,30 +11,23 @@ local function date_time()
     return os.date("%H:%M (%d/%m/%Y)")
 end
 
-local function timeout()
+local function first_timeout()
     local current_min = os.time()
     local next_minute = math.ceil(current_min / 60) * 60
     return next_minute - current_min
 end
 
 local function onInit(btn)
-    local timer = gears.timer {
-        timeout     = timeout(),
-        single_shot = false,
-        call_now    = false,
-        autostart   = true,
-        callback    = function(timer)
+    foundation.schedule {
+        timeout   = first_timeout(),
+        autostart = true,
+        instant   = false,
+        callback  = function(timer)
             button.update(btn, { text = date_time() })
             timer.timeout = 60
             timer:again()
         end
     }
-
-    local kill_cb = function()
-        awesome.kill(timer, awesome.unix_signal.SIGTERM)
-    end
-
-    awesome.connect_signal("exit", kill_cb)
 end
 
 function self:new()
