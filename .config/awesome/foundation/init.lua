cd8208c6a7f6f47f9a3d09c0e2597680b11dd32e
João Freitas <1160907@isep.ipp.pt>
2024-09-11 11:44:57 +0100

    feat(ui): abstract function schedule

---
+++ .config/awesome/foundation/init.lua
@@ -0,0 +1,22 @@
+local timer = require 'gears.timer'
+
+
+--- Schedules the execution of a function.
+---@param args { timeout: number, autostart: boolean | false, instant: boolean | false, continuous: boolean | true, callback: function }
+local function schedule(args)
+    local ref = timer {
+        timeout     = args.timeout,
+        single_shot = type(args.continuous) == 'boolean' and not args.continuous or false,
+        call_now    = type(args.instant) == 'boolean' and args.instant or false,
+        autostart   = type(args.autostart) == 'boolean' and args.autostart or false,
+        callback    = args.callback
+    }
+
+    awesome.connect_signal("exit", function()
+        ref:stop()
+    end)
+end
+
+return {
+    schedule = schedule,
+}
