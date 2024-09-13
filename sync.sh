5b4faeaf479af496f1ee81141cf1581ed59724c3
João Freitas <1160907@isep.ipp.pt>
2024-09-13 11:55:32 +0100

    chore: add aliases

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-dirs=(".config/awesome" ".config/rofi" ".config/picom" ".config/alacritty" ".local/bin" ".fonts")
+dirs=(".config/awesome" ".config/rofi" ".config/picom" ".config/alacritty" ".config/aliases" ".local/bin")
 
 for dir in ${dirs[@]}; do
     sudo mount --bind ~/$dir/ $dir
